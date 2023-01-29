bcc009294e6ed7c75cfba56e1c2540347e312cc5
Naty <natyaith@yahoo.com.br>
2023-01-28 21:34:56 -0300

    embaralhar cartas

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const cartas = document.querySelectorAll('.carta');
 
 let virouCarta = false;
+let bloqueiaTabuleiro = false;
 let primeiraCarta, segundaCarta;
 
 function viraCarta() {
+    if(bloqueiaTabuleiro) return;
+    if (this === primeiraCarta) return;
+
     this.classList.add('flip');
 
     if (!virouCarta) {
@@ -13,32 +17,45 @@ function viraCarta() {
     }
 
     segundaCarta = this;
-    virouCarta = false;
+    bloqueiaTabuleiro = true;
 
     formouPar();
 }
 
 function formouPar() {
-    if(primeiraCarta.dataset.roupa === segundaCarta.dataset.roupa) {
-        desabilitaCartas();
-        return;
-    }
+    let formouPar = primeiraCarta.dataset.roupa === segundaCarta.dataset.roupa
+    formouPar ? desabilitaCartas() : desviraCartas();
 
-    desviraCartas();
 }
 
 
 function desabilitaCartas() {
     primeiraCarta.removeEventListener('click', viraCarta);
     segundaCarta.removeEventListener('click', viraCarta);
+
+    resetaTabuleiro();
 }
 
 function desviraCartas(){
     setTimeout(() => {
         primeiraCarta.classList.remove('flip');
         segundaCarta.classList.remove('flip');
+
+        resetaTabuleiro();
     }, 1500);
 }
+
+function resetaTabuleiro() {
+    [virouCarta, bloqueiaTabuleiro] = [false, false];
+    [primeiraCarta, segundaCarta] = [null, null];
+}
+
+(function embaralhar() {
+    cartas.forEach(carta => {
+        let aleatorio = Math.floor(Math.random()*12);
+        carta.style.order = aleatorio;
+    });
+})();
 
 cartas.forEach(carta => carta.addEventListener ('click', viraCarta));
 
